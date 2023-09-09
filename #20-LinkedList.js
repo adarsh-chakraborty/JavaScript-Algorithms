16898b827c28d962b731e7b5c051b80f6403ee37
Adarsh Chakraborty <adarshchak108@gmail.com>
2023-09-09 19:36:48 +0530

    Linkedlist - Insert, Remove and search

--- #20-LinkedList.js
+++ #20-LinkedList.js
@@ -67,6 +67,106 @@ class LinkedList {
     }
     this.size++;
   }
+
+  // Insert
+  insert(value, index) {
+    // Check if Index is less than 0 or greater than size of the list
+
+    if (index < 0 || index > this.size) {
+      console.log("Invalid index");
+      return;
+    }
+
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    } else {
+      const node = new Node(value);
+      let prev = this.head;
+
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+
+      // Now we have the prev Node
+      // Connect the new node tail to next node
+      node.next = prev.next;
+      // Connect the prev node tail to new node.
+      prev.next = node;
+      this.size++;
+    }
+  }
+
+  removeFrom(index) {
+    if (index < 0 || index > this.size) {
+      return null;
+    }
+
+    let removedNode;
+
+    if (index === 0) {
+      removedNode = this.head;
+      this.head = this.head.next;
+    } else {
+      let prev = this.head;
+
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+
+      const removedNode = prev.next;
+      prev.next = removedNode.next;
+    }
+    this.size--;
+    return removedNode;
+  }
+
+  removeValue(value) {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    if (value === this.head.value) {
+      this.head = this.head.next;
+      this.size--;
+      return value;
+    } else {
+      let prev = this.head;
+
+      while (prev.next && prev.next.value !== value) {
+        // stop just the previous of the node to be removed
+        prev = prev.next;
+      }
+
+      if (prev.next) {
+        const removedNode = prev.next;
+        prev.next = removedNode.next;
+        this.size--;
+        return value;
+      }
+      return null;
+    }
+  }
+
+  // search the value in the linkedlist, return the index if found else -1
+  search(value) {
+    if (this.isEmpty()) {
+      console.log("List is empty.");
+      return -1;
+    } else {
+      let i = 0;
+      let curr = this.head;
+
+      while (curr) {
+        if (curr.value === value) {
+          return i;
+        }
+        curr = curr.next;
+        i++;
+      }
+      return -1;
+    }
+  }
 }
 
 const list = new LinkedList();
@@ -82,6 +182,25 @@ list.append(40);
 list.print();
 list.append(50);
 list.print();
+list.insert(60, 4);
+list.insert(70, 1);
+
+// list.print();
+// list.removeFrom(3);
+// list.removeFrom(0);
+// list.removeFrom(4);
+
+// list.print();
+// list.removeValue(20);
+// list.removeValue(70);
+// list.removeValue(30);
+// list.removeValue(30);
+// list.removeValue(50);
+list.print();
+console.log(list.search(20));
+console.log(list.search(50));
+console.log(list.search(30));
+console.log(list.search(31));
 
 // Time Complexity of Prepend & append
 // prepend: Constant Big O(1)
