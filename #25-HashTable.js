860389b62c30d0af28948b0d7558a43936b05d45
Adarsh Chakraborty <adarshchak108@gmail.com>
2023-09-12 15:23:11 +0530

    Hash Table and (later) double linkedlist

---
+++ #25-HashTable.js
@@ -0,0 +1,65 @@
+/*
+A HashTable also known as HashMap is a data structure that is used to store key value pairs.
+Given a key, you can associate a value for very fast look up.
+
+A Hashing functiona accepts a string key, converts it to a hashcode using a defined logic and then maps it to
+numeric index within the bounds of the array. We use such Hasing Function with a fixed size array.
+
+Implementations:
+- Set to store a key-value pair
+- Get to retrive a given value based on the given key.
+- Remove to delete a key-value pair.
+- Hashing function to convert string key to numeric index.
+
+Hashtable are usually implement where constant time lookup and insertion is required e.g Database Indexing, Caches.
+
+*/
+
+class HashTable {
+  constructor(size) {
+    this.table = new Array(size);
+    this.size = size;
+  }
+
+  hash(key) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+      total += Number(key.charCodeAt(i));
+    }
+    return total % this.size;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    this.table[index] = value;
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    return this.table[index];
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    this.table[index] = undefined;
+  }
+
+  display() {
+    for (let i = 0; i <= this.table.length; i++) {
+      if (this.table[i]) {
+        console.log(i, this.table[i]);
+      }
+    }
+  }
+}
+
+const table = new HashTable(50);
+table.set("Adarsh", "Chakraborty");
+table.set("fruit", "Beer");
+console.log("Adarsh =>", table.get("Adarsh"));
+console.log("adarsh =>", table.get("adarsh"));
+console.log("fruit =>", table.get("fruit"));
+table.display();
+console.log("Removing Fruit");
+table.remove("fruit");
+table.display();
